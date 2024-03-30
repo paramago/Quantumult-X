64ac2ad7a02253e7a95a8f77b53fb42f96378bb7
paramago <52270738+paramago@users.noreply.github.com>
2024-03-31 00:06:01 +0800

    Update notBoringWeather.js

--- write/notBoringWeather.js
+++ write/notBoringWeather.js
@@ -2,7 +2,6 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 if (obj.data) {
-    // obj.offerings[0].identifier = "annualSuper";
     obj.subscriber.entitlements = {
         "com.andyworks.weather.yearlyBeliever":{
             "expires_date":"2029-05-26T05:05:04Z",
@@ -26,39 +25,6 @@ if (obj.data) {
 
 body = JSON.stringify(obj);
 $done({body});
-
-
-
-// {
-//     "placements" : {
-//     "fallback_offering_id" : "default"
-// },
-//     "offerings" : [
-//     {
-//         "metadata" : null,
-//         "description" : "The default offering",
-//         "identifier" : "default",
-//         "packages" : [
-//             {
-//                 "platform_product_identifier" : "com.andyworks.weather.yearlyPatron",
-//                 "identifier" : "annualSuper"
-//             },
-//             {
-//                 "platform_product_identifier" : "com.andyworks.weather.monthBeliever",
-//                 "identifier" : "monthSuper"
-//             },
-//             {
-//                 "platform_product_identifier" : "com.andyworks.weather.yearlyApp",
-//                 "identifier" : "annualApp"
-//             }
-//         ]
-//     }
-// ],
-//     "current_offering_id" : "default"
-// }
-
-
-
 /***/
 
 
