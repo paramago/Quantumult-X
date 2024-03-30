6659926bd7e8bc6b30aed729a90b2ef39f7efeda
paramago <tbistar@hotmail.com>
2024-03-30 15:14:48 +0800

    update contents

--- write/notBoringWeather.js
+++ write/notBoringWeather.js
@@ -3,25 +3,25 @@ var obj = JSON.parse(body);
 
 if (obj.data) {
     obj.offerings[0].identifier = "annualSuper";
-    // obj.subscriber.entitlements = {
-    //     "com.andyworks.weather.yearlyBeliever":{
-    //         "expires_date":"2029-05-26T05:05:04Z",
-    //         "product_identifier":"com.andyworks.weather.yearlyBeliever",
-    //         "purchase_date":"2022-04-09T05:05:04Z"
-    //     }
-    // }
-    // obj.subscriber.subscriptions ={
-    //     "com.andyworks.weather.yearlyBeliever":{
-    //             "billing_issues_detected_at":null,
-    //             "expires_date":"2029-05-26T05:05:04Z",
-    //             "is_sandbox":false,
-    //             "original_purchase_date":"2022-04-09T05:05:04Z",
-    //             "period_type":"normal",
-    //             "purchase_date":"2022-04-09T05:05:04Z",
-    //             "store":"app_store",
-    //             "unsubscribe_detected_at":null
-    //         }
-    //     }
+    obj.subscriber.entitlements = {
+        "com.andyworks.weather.yearlyBeliever":{
+            "expires_date":"2029-05-26T05:05:04Z",
+            "product_identifier":"com.andyworks.weather.yearlyBeliever",
+            "purchase_date":"2022-04-09T05:05:04Z"
+        }
+    }
+    obj.subscriber.subscriptions ={
+        "com.andyworks.weather.yearlyBeliever":{
+                "billing_issues_detected_at":null,
+                "expires_date":"2029-05-26T05:05:04Z",
+                "is_sandbox":false,
+                "original_purchase_date":"2022-04-09T05:05:04Z",
+                "period_type":"normal",
+                "purchase_date":"2022-04-09T05:05:04Z",
+                "store":"app_store",
+                "unsubscribe_detected_at":null
+            }
+        }
 }
 
 console.log("obj.data = ", obj.data);
