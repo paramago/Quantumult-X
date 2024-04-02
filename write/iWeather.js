fb036b0bf0f75195b759acb46170264b346a752c
paramago <tbistar@hotmail.com>
2024-04-03 02:15:45 +0800

    develop iWeather

--- write/iWeather.js
+++ write/iWeather.js
@@ -2,8 +2,8 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.appearances.forEach(item => {
-    item.ispro = 0;
-});
+    item.is_pro = 0;
+})
 
 body = JSON.stringify(obj);
 $done({body});
