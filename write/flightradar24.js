4ae5da810b63ebca84655f7855da8c773d874ac1
paramago <tbistar@hotmail.com>
2024-04-07 10:38:05 +0800

    develop windy test

--- write/flightradar24.js
+++ write/flightradar24.js
@@ -10,7 +10,7 @@ obj.features.map.info.aircraft = "unlimited";
 obj.features.map.filters.receivers = "enabled";
 obj.features.map.hide_header = "enabled";
 
-obj.userData.subscriptions = { "expires_date": null};
+// obj.userData.subscriptions = { "expires_date": null};
 obj.userData.accountType = "Gold";
 obj.userData.features.user.fleets.max = 10;
 obj.userData.features.user.map.filters.categories = "enabled";
